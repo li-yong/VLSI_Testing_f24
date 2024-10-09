b703046745005df8b2b26f998c645c2b2a8ad0ef
YONG LI <sunraise2005@gmail.com>
2024-10-09 10:09:51 -0400

    u

--- py_isc_parser/py_isc_parser.py
+++ py_isc_parser/py_isc_parser.py
@@ -13,7 +13,7 @@ logging.basicConfig(filename='py_isc.log', filemode='a', format='%(asctime)s %(m
 
 # Define a data structure to store gate information
 class Gate:
-    def __init__(self, gate_net_id, gate_id, gate_type, gate_fanout_cnt, gate_fanin_cnt, gate_faults, input_gates_netid_list,fanout_branch_from_netid):
+    def __init__(self, gate_net_id, gate_id, gate_type, gate_fanout_cnt, gate_fanin_cnt, gate_faults, input_gates_netid_list,fanout_branch_from_gateid,output_gates_netid_list=[]):
         self.gate_net_id = gate_net_id
         self.gate_id = gate_id
         self.gate_type = gate_type
@@ -21,11 +21,12 @@ class Gate:
         self.gate_fanin_cnt = gate_fanin_cnt
         self.gate_faults = gate_faults
         self.input_gates_netid_list = input_gates_netid_list
-        self.fanout_branch_from_netid = fanout_branch_from_netid
+        self.fanout_branch_from_gateid = fanout_branch_from_gateid
+        self.output_gates_netid_list = output_gates_netid_list
     
 
     def __repr__(self):
-        return f"Net ID::{self.gate_net_id}, Gate ID: {self.gate_id}, Type: {self.gate_type}, InputsCnt: {self.gate_fanin_cnt}, OutputsCnt: {self.gate_fanout_cnt}, Faults: {self.gate_faults}"
+        return f"netid:{self.gate_net_id}, gate_id: {self.gate_id}, type: {self.gate_type}, InputsCnt: {self.gate_fanin_cnt}, OutputsCnt: {self.gate_fanout_cnt}, Faults: {self.gate_faults}, InputGateNetList: {self.input_gates_netid_list},OutputGateNetList:{self.output_gates_netid_list}, fanout_branch_from_gateid: {self.fanout_branch_from_gateid}" 
 
 # Function to parse the netlist
 def parse_netlist(content):
@@ -50,7 +51,7 @@ def parse_netlist(content):
 
         #split line by space   
         line = line.strip()
-        print(line)
+        # print(line)
         line = re.sub(r'\s+', ' ', line) #replace multiple spaces with single space
         groups = line.split(" ")
 
@@ -60,6 +61,7 @@ def parse_netlist(content):
         gate_faults=[]
         input_gates_netid_list = []
         fanout_branch_from_netid=None
+        output_gates_netid_list=[]
 
         #Handle from gate (wire)
         if gate_id.find('fan')>0 and gate_type in ('from'):
@@ -91,33 +93,96 @@ def parse_netlist(content):
                 
 
             
-            # Create a gate object
-            gate = Gate(gate_net_id, gate_id, gate_type, gate_fanout_cnt, gate_fanin_cnt, gate_faults, input_gates_netid_list,fanout_branch_from_netid)
-            gates.append(gate)
+        # Create a gate object. both `from` or `regular` gate
+        gate = Gate(gate_net_id, gate_id, gate_type, gate_fanout_cnt, gate_fanin_cnt, gate_faults, input_gates_netid_list,fanout_branch_from_netid,output_gates_netid_list)
+        gates.append(gate)
     
     return gates
 
-# Print the circuit structure (gates and their connections)
-def print_circuit_structure(gates):
+# Find a gate by its netid
+def find_gate_by_netid(gates, netid):
     for gate in gates:
-        print(gate)
+        if gate.gate_net_id == netid:
+            # return gates.index(gate)
+            return gate
+    return None
+
+# Find a gate by its gate_id
+def find_gate_by_gateid(gates, gate_id):
+    for gate in gates:
+        if gate.gate_id == gate_id:
+            #return the index of the gate
+            # return gates.index(gate)
+            return gate
+    return None
+
+
+# Print the circuit structure (gates and their connections)
+def populate_output_gates(gates):
+    for gate in gates:
+        parent_gate = None
+        # print(gate)
+       
+        # if gate.gate_type == "from":
+        #     parent_gate = find_gate_by_gateid(gates, gate.fanout_branch_from_gateid)
+        #     parent_gate.output_gates_netid_list.append(gate.gate_net_id)
+        #     print(f"{parent_gate.gate_id} -> Fanout Branch {gate.gate_id}")
+
+        
+        if gate.gate_fanin_cnt==0:
+            continue
+
+        #loop the input gates netid list
+        for input_gate_netid in gate.input_gates_netid_list:
+            parent_gate = find_gate_by_netid(gates, input_gate_netid)
+            # parent_gate = gates[parent_gate_index]
+
+            if parent_gate.gate_type == "from":
+                parent_gate = find_gate_by_gateid(gates, parent_gate.fanout_branch_from_gateid)
+                # parent_gate = gates[parent_gate_index]
+                # print(f"Wire {gate.gate_id} -> {gate.fanout_branch_from_gateid}")
+
+            # tmp = parent_gate.output_gates_netid_list
+            # tmp.append(gate.gate_net_id)
+            # parent_gate.output_gates_netid_list = tmp
+
+
+            parent_gate.output_gates_netid_list.append(gate.gate_net_id)
+
+            # print(f"updated output gate, {parent_gate.gate_id} -> {gate.gate_id}")
+    # print("output gate populated")
+    return gates
+
+
+
+def print_circuit_output(gates):
+    for gate in gates:
+        if gate.gate_type == "from":
+            continue
+        # if gate.gate_fanin_cnt>0:
+            # print("TO: "+",".join(gate.input_gates_netid_list) +" -> "+gate.gate_id)
+        if gate.gate_fanout_cnt>0:
+            print("FROM: " +gate.gate_net_id+" TO "+",".join(gate.output_gates_netid_list))
 
 def save_to_json(gates, output_file):
     json_data = []
-    for line_number, gate in gates:
+    for gate in gates:
         gate_dict = {
-            "line_number": line_number,
+            "line_number": gate.gate_net_id,
+            "net_id": gate.gate_net_id,
             "identifier": gate.gate_id,
             "gate_type": gate.gate_type,
-            "fanout": int(gate.outputs),
-            "fanin": int(gate.inputs) if gate.inputs.isdigit() else 0,
-            "inputs": [int(gate.inputs)] if gate.inputs.isdigit() else [],
-            "faults": gate.faults.split() if gate.faults != "None" else []
+            "fanout": gate.gate_fanout_cnt,
+            "fanin": gate.gate_fanin_cnt,
+            "inputs": gate.input_gates_netid_list,
+            "output": gate.output_gates_netid_list,
+            "faults": gate.gate_faults,
         }
         json_data.append(gate_dict)
     
     with open(output_file, 'w') as f:
         json.dump(json_data, f, indent=4)
+        print("Circuit Json Output saved to", output_file)
 
 
 # Example usage
@@ -155,8 +220,10 @@ if __name__ == "__main__":
     if action == "parse_to_json":
         # Parse the netlist
         gates = parse_netlist(netlist_content)
+        gates = populate_output_gates(gates)
 
         # Print the circuit structure
-        print_circuit_structure(gates)
+        # print_circuit_structure(gates)
+        print_circuit_output(gates)
 
-        save_to_json(gates, 'output.json')
+        save_to_json(gates, out_json)
